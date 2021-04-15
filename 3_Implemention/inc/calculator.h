ed64d4dd09743d59e798352418e30faf105d8cbe
295557 <80755178+295557@users.noreply.github.com>
2021-04-15 18:34:24 +0530

    Update calculator.h

--- 3_Implemention/inc/calculator.h
+++ 3_Implemention/inc/calculator.h
@@ -3,66 +3,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-/**
-*  adds the operand1 and operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 + operand2
-*/
+
 void addition();
 
-/**
-*  subtracts the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 - operand2
-*/
+
 void subtraction();
 
-/**
-*  multiply the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 * operand2
-*/
+
 void multiplication();
 
 
-/**
-* divides the operand1 by operand2 and returns the result 
-* @param[in] operand1 
-* @param[in] operand2 
-* @return integer value of the operand1 / operand2
-*/
+
 void division();
 
 
-/**
-* modulus the operand1 by operand2 and returns the result 
-* @param[in] operand1 
-* @param[in] operand2 
-* @return integer value of the operand1 % operand2
-*/
+
 void modulus();
 
-/**
-*  squares the operand1 and returns the result
-* @param[in] operand1 
-* @return Result of operand1 ^2
-*/
+
 
 void square();
 
-/**
-*  cubes the operand1 and returns the result
-* @param[in] operand1 
-* @return Result of operand1 ^3
-*/
+
 void cube();
 
-/**
-*  finds the square root of the operand1 and returns the result
-* @param[in] operand1 
-* @return Result of square root of operand1
-*/
+
 void squareroot();
