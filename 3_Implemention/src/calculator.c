3a1b37b9cc49a5f77f613fd04f75a3f3c749decf
295557 <80755178+295557@users.noreply.github.com>
2021-06-16 13:17:47 +0530

    Update calculator.c

--- 3_Implemention/src/calculator.c
+++ 3_Implemention/src/calculator.c
@@ -53,3 +53,62 @@ void squareroot(){
     double s = sqrt(b);
     printf("The square root of %lf is %lf",b,s);
 }
+
+double simple_interest(){
+	int p,t;
+ 
+        float r,si,amount,ci;
+ 
+        printf("Please enter principal,time and rate of interest");
+ 
+        scanf("%d%d%f",&p,&t,&r);
+ 
+        si=p*t*r/100;
+ 
+        printf("\nSimple interest = %.3f",si);
+}
+
+double compound_interest(){
+	int p,t;
+ 
+        float r,si,amount,ci;
+ 
+        printf("Please enter principal,time and rate of interest");
+ 
+        scanf("%d%d%f",&p,&t,&r);
+ 
+        amount=p*pow((1 +r/100),t);
+ 
+        ci=amount-p;
+ 
+        printf("\nCompound interest = %.3f",ci);
+ 
+}
+double quadratic_equation()
+{
+	double a, b, c, discriminant, root1, root2, realPart, imagPart;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    discriminant = b * b - 4 * a * c;
+
+    // condition for real and different roots
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
+    }
+
+    // condition for real and equal roots
+    else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %.2lf;", root1);
+    }
+
+    // if roots are not real
+    else {
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-discriminant) / (2 * a);
+        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imagPart, realPart, imagPart);
+    }
+}
